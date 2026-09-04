ffdc2af066414af625b8503f7a79961ec0f23b13
Janek <jasiogamer@gmail.com>
2026-09-04 23:31:21 +0200

    elements for save var

---
+++ Editor/Extensions/SerializedProppertyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+namespace fefek5.Toys.Editor.Extensions
+{
+    public static class SerializedProppertyExtensions
+    {
+        public static IEnumerable<SerializedProperty> GetVisibleChildren(this SerializedProperty property)
+        {
+            var iterator = property.Copy();
+            var end = property.GetEndProperty();
+            var enterChildren = true;
+
+            while (iterator.NextVisible(enterChildren) &&
+                   !SerializedProperty.EqualContents(iterator, end))
+            {
+                yield return iterator.Copy();
+                enterChildren = false;
+            }
+        }
+    }
+}
---
+++ Editor/VisualElements/FoldoutElement.cs
@@ -0,0 +1,32 @@
+﻿using fefek5.Toys.Editor.Extensions;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace fefek5.Toys.Editor.VisualElements
+{
+    public class FoldoutElement : Foldout
+    {
+        public VisualElement Header { get; private set; }
+        
+        public FoldoutElement(SerializedProperty property, VisualElement header =  null)
+            : this(property.displayName, header)
+        {
+            this.BindProperty(property);
+            
+            foreach (var child in property.GetVisibleChildren()) 
+                Add(new PropertyField(child));
+        }
+
+        public FoldoutElement(string label, VisualElement header =  null)
+        {
+            text = label;
+
+            if (header == null) return;
+            
+            Header = header;
+            
+            hierarchy[0].hierarchy[0].Add(header);
+        }
+    }
+}
